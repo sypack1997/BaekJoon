3e4ac094120d06456c01edc71844f86e7fe899af
sypack1997 <sypack1997@naver.com>
2022-10-04 22:52:51 +0900

    upload_26

--- 정렬/10989시간초과.py
+++ 정렬/10989시간초과.py
@@ -1,4 +1,21 @@
 import sys
+n = int(sys.stdin.readline())
+num_list = [0] * 10001
+
+for _ in range(n):
+    num_list[int(sys.stdin.readline())] += 1 
+
+for i in range(10001):
+    if num_list[i] != 0:
+        for j in range(num_list[i]):
+            print(i)
+
+
+
+
+
+'''
+import sys
 num_list = []
 
 for _ in range(int(sys.stdin.readline())):
@@ -8,3 +25,4 @@ num_list.sort()
 
 for i in num_list:
     print(i)
+'''
